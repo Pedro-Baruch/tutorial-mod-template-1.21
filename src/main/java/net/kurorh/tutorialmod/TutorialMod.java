806f19b267a612cb5964497ef86804ac4cb4c13b
Pedro Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2025-02-20 16:18:04 -0300

    Adding two new blocks

--- src/main/java/net/kurorh/tutorialmod/TutorialMod.java
+++ src/main/java/net/kurorh/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.kurorh.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kurorh.tutorialmod.block.ModBlocks;
 import net.kurorh.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
